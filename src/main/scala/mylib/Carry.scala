6dede249b56ed18cac9cad732d90786b9720f40e
lawrie <lawrie.griffiths@ntlworld.com>
2019-04-04 08:29:09 +0100

    multiple pin PWM

---
+++ src/main/scala/mylib/Carry.scala
@@ -0,0 +1,29 @@
+package mylib
+
+import spinal.core._
+
+class CarryAdder(size : Int) extends Component{
+  val io = new Bundle{
+    val a = in UInt(size bits)
+    val b = in UInt(size bits)
+    val result = out UInt(size bits)      //result = a + b
+  }
+
+  var c = False                   //Carry, like a VHDL variable
+  for (i <- 0 until size) {
+    //Create some intermediate value in the loop scope.
+    val a = io.a(i)
+    val b = io.b(i)
+
+    //The carry adder's asynchronous logic
+    io.result(i) := a ^ b ^ c
+    c \= (a & b) | (a & c) | (b & c);    //variable assignment
+  }
+}
+
+
+object CarryAdderProject {
+  def main(args: Array[String]) {
+    SpinalVerilog(new CarryAdder(4))
+  }
+}
--- src/main/scala/mylib/PWMTest.scala
+++ src/main/scala/mylib/PWMTest.scala
@@ -2,33 +2,37 @@ package mylib
 
 import spinal.core._
 
-class PWM extends Component {
+class PWM(width: Int) extends Component {
   val io = new Bundle {
-    val pin = out Bool
-    val duty = in UInt(8 bits)
+    val pins = out Bits(width bits)
+    val duty = in Vec(UInt(8 bits), width)
   }
 
-  val counter = Reg(UInt(8 bits))
+  for(i <- 0 until width) {
+    val counter = Reg(UInt(8 bits))
 
-  counter := counter + 1
+    counter := counter + 1
 
-  io.pin := counter <= io.duty && !(io.duty === 0)
+    io.pins(i) := counter <= io.duty(i) && io.duty(i) =/= 0
+  }
 }
 
-class PWMTest extends Component {
+class PWMTest(width: Int) extends Component {
   val io = new Bundle {
-    val pin = out Bool
+    val pins = out Bits(width bits)
   }
 
-  val pwm = new PWM
-  pwm.io.pin <> io.pin
-  pwm.io.duty := 127
+  val pwm = new PWM(width)
+  pwm.io.pins <> io.pins
 
+  for (i <- 0 until width) {
+    pwm.io.duty(i) := i * 4
+  }
 }
 
 object PWMTest {
   def main(args: Array[String]) {
-    SpinalVerilog(new PWMTest)
+    SpinalVerilog(new PWMTest(8))
   }
 }
 
